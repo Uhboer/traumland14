14c06e7b0d6d33c8b2bb647c57364105142648e0
docnite <docnite0530@gmail.com>
2024-11-11 23:30:18 +0300

    Create StyleLora sheet; Added 'Lora' fonts

--- Content.Client/Stylesheets/StylesheetManager.cs
+++ Content.Client/Stylesheets/StylesheetManager.cs
@@ -1,6 +1,7 @@
 using Robust.Client.ResourceManagement;
 using Robust.Client.UserInterface;
 using Robust.Shared.IoC;
+using Content.Client._DA.Stylesheets;
 
 namespace Content.Client.Stylesheets
 {
@@ -11,13 +12,15 @@ namespace Content.Client.Stylesheets
 
         public Stylesheet SheetNano { get; private set; } = default!;
         public Stylesheet SheetSpace { get; private set; } = default!;
+        public Stylesheet SheetLora { get; private set; } = default!;
 
         public void Initialize()
         {
             SheetNano = new StyleNano(_resourceCache).Stylesheet;
             SheetSpace = new StyleSpace(_resourceCache).Stylesheet;
+            SheetLora = new StyleLora(_resourceCache).Stylesheet;
 
-            _userInterfaceManager.Stylesheet = SheetNano;
+            _userInterfaceManager.Stylesheet = SheetLora;
         }
     }
 }
