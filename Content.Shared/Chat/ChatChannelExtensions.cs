45c45231e51ef3702ba6ca820fda36c0d447c2f8
docnite <docnite0530@gmail.com>
2025-04-09 00:13:57 +0300

    HUD redesign 3

--- Content.Shared/Chat/ChatChannelExtensions.cs
+++ Content.Shared/Chat/ChatChannelExtensions.cs
@@ -6,6 +6,7 @@ public static class ChatChannelExtensions
     {
         return channel switch
         {
+            /*
             ChatChannel.Server => Color.FromHex("#D2691E"),
             ChatChannel.Radio => Color.FromHex("#556B2F"),
             ChatChannel.LOOC => Color.FromHex("#4682B4"),
@@ -15,6 +16,16 @@ public static class ChatChannelExtensions
             ChatChannel.AdminAlert => Color.FromHex("#B22222"),
             ChatChannel.AdminChat => Color.FromHex("#FF1493"),
             ChatChannel.Whisper => Color.FromHex("#505050"),
+            */
+            ChatChannel.Server => Color.Orange,
+            ChatChannel.Radio => Color.LimeGreen,
+            ChatChannel.LOOC => Color.MediumTurquoise,
+            ChatChannel.OOC => Color.LightSkyBlue,
+            ChatChannel.Dead => Color.MediumPurple,
+            ChatChannel.Admin => Color.Red,
+            ChatChannel.AdminAlert => Color.Red,
+            ChatChannel.AdminChat => Color.HotPink,
+            ChatChannel.Whisper => Color.DarkGray,
             _ => Color.LightGray
         };
     }
