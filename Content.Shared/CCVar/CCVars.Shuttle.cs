33ee923309de761a47f840ef74ccdc85668e7345
docnite <docnite0530@gmail.com>
2025-06-01 22:21:30 +0300

    Disable shuttle arriavals by default

--- Content.Shared/CCVar/CCVars.Shuttle.cs
+++ Content.Shared/CCVar/CCVars.Shuttle.cs
@@ -24,7 +24,7 @@ public sealed partial class CCVars
     /// Whether the arrivals shuttle is enabled.
     /// </summary>
     public static readonly CVarDef<bool> ArrivalsShuttles =
-        CVarDef.Create("finster_shuttle.arrivals", true, CVar.SERVERONLY);
+        CVarDef.Create("finster_shuttle.arrivals", false, CVar.SERVERONLY);
 
     /// <summary>
     /// The map to use for the arrivals station.
